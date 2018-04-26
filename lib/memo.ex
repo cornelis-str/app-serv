367107222f1e88cf2d73fba280e439d2b523a4ff
anee <anee@kth.se>
2018-04-26 15:15:34 +0200

    more refaktoring, updated half-done room_data_handler

--- lib/memo.ex
+++ lib/memo.ex
@@ -31,12 +31,12 @@ defmodule Memo do
   # }
 
   # Hämtar och ändrar user data på begäran.
-  def start(file_path) do
+  def start() do
     # Ta bort kommentering i lib/application.ex för att det ska fungera
 
-    {:ok, pid} = Task.Supervisor.start_child(Memo.TaskSupervisor, fn -> memo_mux([]) end)
+    {:ok, pid} = Task.Supervisor.start_child(Memo.TaskSupervisor, fn -> memo_mux([], []) end)
     Process.register(pid, :memo_mux)
-    spawn(fn -> file_mux(file_path) end) |> Process.register(:file_mux)
+    #spawn(fn -> file_mux(file_path) end) |> Process.register(:file_mux)
   end
 
   def memo_mux(user_pid_list, room_pid_list) do
@@ -50,7 +50,7 @@ defmodule Memo do
             send user_pid_list[user_id], action
             memo_mux new_user_pid, room_pid_list
 
-          nil when method == :set ->
+          nil when method == :add ->
             new_user_pid = user_pid_list |> Map.put(user_id, spawn fn -> load_user(user_id) |> user_data_handler() end)
             send user_pid_list[user_id], action
             memo_mux new_user_pid, room_pid_list
@@ -97,23 +97,23 @@ defmodule Memo do
 
   def user_data_handler(user_data) do
     receive do
-      {:get, pid, thing, {:user_id}} ->
+      {:get, pid, {:user_id}} ->
         send pid, user_data.user_id
         user_data_handler(user_data)
 
-      {:get, pid, thing, {:notifs}} ->
+      {:get, pid, {:notifs}} ->
         send pid, user_data.notifs
         user_data_handler(user_data)
 
-      {:get, pid, thing, {:firends, user_id}} ->
+      {:get, pid, {:firends, user_id}} ->
         get_friend user_data, user_id, pid
         user_data_handler(user_data)
 
-      {:get, pid, thing, {:rooms, room_id}} ->
-        get_rooms user_data, pid           #TODO implementera get_rooms
+      {:get, pid,{:room_list}} ->
+        get_room_list user_data, pid           #TODO implementera get_rooms
         user_data_handler(user_data)
 
-      {:get, pid, thing, {:has_new}} ->
+      {:get, pid,{:has_new}} ->
         send pid, user_data.has_new
         user_data_handler(user_data)
 
@@ -144,34 +144,60 @@ defmodule Memo do
     end
   end
 
+  def room_data_handler(room_data) do
+    receive do
+      {:get, pid, {:room, room_part}} ->
+        get_room room_data, room_part, pid
+
+      {:get, pid, {:quest, quest_id}} ->
+        get_quest room_data, quest_id, pid          #TODO updatera för att använda room_map
+
+      {:get, pid, {:quest_pic, resource_id}} ->
+        get_quest_pics room_data, resource_id, pid  #TODO updatera för att använda room_map
+
+      {:set, pid, {:room, which_room_part, part_to_add, how}} ->
+        set_room room_data, how, which_room_part, part_to_add, pid
+
+      {:set, pid, {:quest, quest_id, quest, how}} ->
+        set_quest room_data, how, quest_id, quest pid #TODO updatera för att använda room_map
+
+      {:set, pid, {:quest_pic, resource_id, resource, how}} ->
+        set_quest_pics room_data, how, resource_id, resource, pid #TODO updatera för att använda room_map
+
+      {:set, pid, {:room, :del}} ->
+        "Code for deletion of room here" #TODO
+    end
+  end
+
   def get_friend(user_data, user_id, pid) do
     friend = (user_data.friends |> Enum.find(fn({:friend, %{:user_id => x, :friends => _}}) -> x == user_id end))
     send pid, friend
   end
 
-  #TODO implement
-  def get_rooms(user_data, pid) do end
-
-  #TODO använd room_data/room_map istället för user_data/user_data
-  def get_room(user_data, room_id, pid) do
-    room = (user_data.rooms |> Enum.find(fn(%{:room_id => x, :room => _}) -> x == room_id end))
-    send pid, room
+  def get_room_list(user_data, pid) do
+    send pid, {:memo, user_data.rooms}
   end
 
-  #TODO använd room_data/room_map istället för user_data/user_data
-  def get_quest(user_data, resource_id, pid) do
-    [username, roomname, _] = String.split(resource_id, "@")
-    room = (user_data.rooms |> Enum.find(fn(%{:room_id => x, :room => _}) -> x == "#{username}@#{roomname}" end))
-    quest = (room.quests |> Enum.find(fn(%{:quest_id => x, :quest => _}) -> x == resource_id end))
-    send pid, quest
+  def get_room(room_data, room_part, pid) do
+    case room_part do
+      :owner -> send pid, room_data.owner
+      :name -> send pid, room_data.name
+      :topic -> send pid, room_data.topic
+      :icon -> send pid, room_data.icon
+      :users -> send pid, room_data.users
+      :quests -> send pid, room_data.quests
+      :quest_pics -> send pid, room_data.quest_pics
+    end
   end
 
-  #TODO använd room_data/room_map istället för user_data/user_data
-  def get_quest_pics(user_data, resource_id, pid) do
-    [username, roomname, _, _, _] = String.split(resource_id, "@")
-    room = (user_data.rooms |> Enum.find(fn(%{:room_id => x, :room => _}) -> x == "#{username}@#{roomname}" end))
-    quest_pic = (room.quest_pics |> Enum.find(fn(%{:quest_pic_id => x, :pic => _}) -> x == resource_id end))
-    send pid, quest_pic
+  def get_quest(room_data, quest_id, pid) do
+    send pid, room_data.quests
+    |> Enum.find(fn(%{:quest_id => ^quest_id, :quest => _}) -> true end)
+  end
+
+  def get_quest_pics(room_data, quest_pic_id, pid) do
+    send pid, room_data.quest_pics
+    |> Enum.find(fn(%{:quest_pic_id => ^quest_pic_id, :pic => _}) -> true end)
   end
 
   def set_notif(user_data, method, notif, pid) do
@@ -216,15 +242,11 @@ defmodule Memo do
   end
 
   # TODO implementera
-  def set_rooms(user_data, pid) do end
+  def set_rooms(user_data, how, pid) do end
 
   #TODO använd room_data/room_map istället för user_data/user_data
-  def set_room(user_data, method, room_id, room_part, part, pid) do
-    user_data.rooms
-    |> Enum.find(fn(%{:room_id => ^room_id, :room => _}) -> true end)
-    |> case do
-      nil when method == :add -> user_data |> Map.replace!(:rooms, [part | user_data.rooms])
-
+  def set_room(room_data, how, room_part, part, pid) do
+    case room_part do
       nil when method == :del -> send pid, {:memo, "Can't delete nonexisting"}
 
       %{:room_id => _, :room => _} ->
@@ -345,25 +367,8 @@ defmodule Memo do
     end
   end
 
-  def room_data_handler(room_data) do
-    receive do
 
-      {:get, pid, {:quest, quest_id}} ->
-        get_quest room_data, quest_id, pid          #TODO updatera för att använda room_map
-
-      {:get, pid, {:quest_pic, resource_id}} ->
-        get_quest_pics room_data, resource_id, pid  #TODO updatera för att använda room_map
-
-      {:set, pid, {:quest, quest_id, what}} ->
-        set_quest room_data, what, quest_id, value, pid #TODO updatera för att använda room_map
-
-      {:set, pid, {:quest_pic, resource_id, what}} ->
-        set_quest_pics room_data, what, resource_id, value, pid #TODO updatera för att använda room_map
-
-    end
-
-  end
-
+  # TODO gör klar
   # Sparar till och laddar från fil
   def file_mux(file_path) do
     file_path |> Path.expand() |> File.stream!([], :line)
